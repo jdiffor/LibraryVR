d36576cca79b887b234f613b711f03f2677bebe6
John Diffor <jwdiffor@gmail.com>
2018-12-21 01:26:48 -0600

    Rewrote project so it no longer relies on buggy aframe template

--- components/set-image.js
+++ components/set-image.js
@@ -1,71 +1,32 @@
-/* global AFRAME */
 
-/**
- * Component that listens to an event, fades out an entity, swaps the texture, and fades it
- * back in.
- */
-AFRAME.registerComponent('set-image', {
-  schema: {
-    on: {type: 'string'},
-    target: {type: 'selector'},
-    src: {type: 'string'},
-    dur: {type: 'number', default: 800}
-  },
+function changeImage(direction) {
 
-  init: function () {
-    var data = this.data;
-    var el = this.el;
+    if(current[direction] != null) {
+        // Fade out image.
+        console.log(direction);
 
-    this.setupFadeAnimation();
+        // Wait for fade to complete.
+        setTimeout(function () {
+            // Set image.
+            document.querySelector('#image-current').setAttribute('material', 'src', current[direction].img);
 
-    el.addEventListener(data.on, function () {
-        if(current[data.src.split('-')[1]] != null) {
-            // Fade out image.
-            data.target.emit('set-image-fade');
-            console.log(data);
-            console.log(el);
-            // Wait for fade to complete.
-            setTimeout(function () {
-                // Set image.
-                data.target.setAttribute('material', 'src', current[data.src.split('-')[1]].img);
+            var previous = current;
+            current = current[direction];
 
+            if(current.right == null)document.querySelector('#goRightLink').setAttribute("visible", false);
+            else document.getElementById('goRightLink').setAttribute("visible", true);
+            if(current.left == null) document.getElementById('goLeftLink').setAttribute("visible", false);
+            else document.getElementById('goLeftLink').setAttribute("visible", true);
+            if(current.forward == null) document.getElementById('goForwardLink').setAttribute("visible", false);
+            else document.getElementById('goForwardLink').setAttribute("visible", true);
+            if(current.backward == null) document.getElementById('goBackwardLink').setAttribute("visible", false);
+            else document.getElementById('goBackwardLink').setAttribute("visible", true);
 
-                var previous = current;
-                current = current[data.src.split('-')[1]];
+            // Once image has been swapped, fade back in
+            document.querySelector('#image-current').emit('endFade');
+        }, 800);
 
-                if(current.right == null)document.querySelector('#goRightLink').setAttribute("visible", false);
-                else document.getElementById('goRightLink').setAttribute("visible", true);
-                if(current.left == null) document.getElementById('goLeftLink').setAttribute("visible", false);
-                else document.getElementById('goLeftLink').setAttribute("visible", true);
-                if(current.forward == null) document.getElementById('goForwardLink').setAttribute("visible", false);
-                else document.getElementById('goForwardLink').setAttribute("visible", true);
-                if(current.backward == null) document.getElementById('goBackwardLink').setAttribute("visible", false);
-                else document.getElementById('goBackwardLink').setAttribute("visible", true);
-
-            }, data.dur);
-        }
-    });
-  },
-
-  /**
-   * Setup fade-in + fade-out.
-   */
-  setupFadeAnimation: function () {
-    var data = this.data;
-    var targetEl = this.data.target;
-
-    // Only set up once.
-    if (targetEl.dataset.setImageFadeSetup) { return; }
-    targetEl.dataset.setImageFadeSetup = true;
-
-    // Create animation.
-    targetEl.setAttribute('animation__fade', {
-      property: 'material.color',
-      startEvents: 'set-image-fade',
-      dir: 'alternate',
-      dur: data.dur,
-      from: '#FFF',
-      to: '#000'
-    });
-  }
-});
+        // Fade image out
+        document.querySelector('#image-current').emit('startFade');
+    }
+}
